8e3a0831806cd6af81b11994a137b625770f8943
Mike Macaulay <michael.macaulay@aviture.us.com>
2013-07-13 11:54:59 -0500

    Adding context to AsyncSpec so you can now say (for example) this.foo = "bar" in your beforeEach and then use this.foo in you 'it' block

--- src/jasmine.async.js
+++ src/jasmine.async.js
@@ -3,13 +3,13 @@ this.AsyncSpec = (function(global){
   // Private Methods
   // ---------------
   
-  function runAsync(block){
+  function runAsync(block,ctx){
     return function(){
       var done = false;
       var complete = function(){ done = true; };
 
       runs(function(){
-        block(complete);
+        block.call(ctx,complete);
       });
 
       waitsFor(function(){
@@ -29,18 +29,19 @@ this.AsyncSpec = (function(global){
   // ----------
 
   AsyncSpec.prototype.beforeEach = function(block){
-    this.spec.beforeEach(runAsync(block));
+    this.spec.beforeEach(runAsync(block,this.spec));
   };
 
   AsyncSpec.prototype.afterEach = function(block){
-    this.spec.afterEach(runAsync(block));
+    this.spec.afterEach(runAsync(block,this.spec));
   };
 
   AsyncSpec.prototype.it = function(description, block){
     // For some reason, `it` is not attached to the current
     // test suite, so it has to be called from the global
     // context.
-    global.it(description, runAsync(block));
+
+    global.it(description, runAsync(block,this.spec));
   };
 
   return AsyncSpec;
